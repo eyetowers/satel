9c7edf6deabf8ca8d3f8b4a9d5228cd7036cb8b5
Saikat Taleb <saikat.taleb@eyetowers.cz>
2024-04-30 15:25:01 +0200

    make frame func more efficient

--- frame.go
+++ frame.go
@@ -1,38 +1,50 @@
 package satel
 
-import "math/bits"
+import (
+	"math/bits"
+)
 
-const seed uint16 = 0x147A
+const (
+	crcSeed uint16 = 0x147A
+
+	reserveBytes int = 4
+	crcBytes     int = 2
+	cmdBytes     int = 1
+)
 
 func frame(data ...byte) []byte {
-	crc := crc(data)
-	f := append(data, crc...)
-	f = replaceSpecialByte(f)
-	f = append([]byte{0xFE, 0xFE}, f...)
-	return append(f, 0xFE, 0x0D)
+	preamble := []byte{0xFE, 0xFE}
+	postamble := []byte{0xFE, 0x0D}
+
+	buf := make([]byte, 0, (cmdBytes + len(data) + crcBytes + cmdBytes + reserveBytes))
+	buf = append(buf, preamble...)
+	buf = appendWithSpecialByte(buf, data...)
+	buf = appendWithSpecialByte(buf, crc(data)...)
+
+	return append(buf, postamble...)
+
 }
 
 func crc(data []byte) []byte {
-	c := seed
+	c := crcSeed
 	for _, b := range data {
 		c = update(c, b)
 	}
 	return []byte{byte(c >> 8), byte(c & 0xFF)}
 }
+
 func update(c uint16, b byte) uint16 {
 	c = bits.RotateLeft16(c, 1)
 	c ^= 0xFFFF
 	return c + c>>8 + uint16(b)
 }
 
-func replaceSpecialByte(input []byte) []byte {
-	var output []byte
-	for i := 0; i < len(input); i++ {
-		if input[i] == 0xFE {
-			output = append(output, 0xFE, 0xF0)
-		} else {
-			output = append(output, input[i])
+func appendWithSpecialByte(buf []byte, data ...byte) []byte {
+	for _, b := range data {
+		buf = append(buf, b)
+		if b == 0xFE {
+			buf = append(buf, 0xF0)
 		}
 	}
-	return output
+	return buf
 }
