3d759f97aebb1bc61e928c5b7d9c99f8b48edd1a
Saikat Taleb <saikat.taleb@eyetowers.cz>
2024-05-06 11:59:37 +0200

    remove spacial byte from incoming bytes

--- scanner.go
+++ scanner.go
@@ -30,9 +30,36 @@ func scan(data []byte, _ bool) (advance int, token []byte, err error) {
 		return i + startIndex + 2, nil, nil
 	}
 	if index > 0 {
-		return i + index + 2, data[:index], nil
+		filteredData, err := removeSpacialByte(data[:index])
+		if err != nil {
+			return 0, nil, err
+		}
+
+		return i + index + 2, filteredData, nil
 	}
 	return 0, nil, nil
+}
+
+func removeSpacialByte(bytes []byte) ([]byte, error) {
+	i := 0
+	j := 0
+	for ; i < len(bytes)-1; i++ {
+		bytes[j] = bytes[i]
+
+		if bytes[i] == 0xFE {
+			i++
+			if bytes[i] != 0xF0 {
+				return nil, ErrCurruptedResponse
+			}
+		}
+		j++
+	}
+
+	if bytes[i] == 0xFE {
+		return nil, ErrCurruptedResponse
+	}
+	bytes[j] = bytes[i]
+	return bytes[:j+1], nil
 }
 
 func IsBusy(bytes ...byte) bool {
