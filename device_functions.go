50b4c017221645834f4dcdac31933290aa4eb146
Saikat <93448463+tsaikat@users.noreply.github.com>
2024-10-09 12:29:16 +0200

    Add mono and bi output function definition (#18)
    This simply adds mono and bi outout function definition.

--- device_functions.go
+++ device_functions.go
@@ -3,5 +3,7 @@ package satel
 type OutputFunction byte
 
 const (
-	NotUsed OutputFunction = 0x00
+	NotUsed    OutputFunction = 0x00
+	MonoSwitch OutputFunction = 0x18
+	BiSwitch   OutputFunction = 0x19
 )
